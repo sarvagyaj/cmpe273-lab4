a3ad3d9b0dcc9df038cf53c3e5653b937b47b715
sarvagyaj <sarvagyaj@gmail.com>
2013-11-09 23:29:53 -0800

    made an Ajax call to the RESTful API, to disable the button and change the status of the book on the webpage.

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,28 @@
-$(":button").click(function() {
-	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
+$(function() {
+	updateStatus();
 });
+
+$(":button").click(function() {
+	var btn = $(this), tr = btn.closest('tr'), isbn = tr.attr("data-id");
+	alert('About to report lost on ISBN ' + isbn);
+
+	$.ajax({
+		url : '/library/v1/books/' + isbn + '?status=lost',
+		type : 'PUT',
+		contentType : 'application/json',
+		success : function(result) {
+			$('#' + isbn).find('.status').html('lost');
+			updateStatus();
+		}
+	})
+});
+
+function updateStatus() {
+	$('tr').each(function() {
+		if ($(this).find('.status').text() == "lost") {
+			$(this).find('button').prop("disabled", "true");
+		} else {
+			$(this).find('button').removeAttr("disabled");
+		}
+	});
+}
